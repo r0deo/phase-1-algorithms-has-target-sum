aadd9a83141307650ffdbef4102cbbee189607af
Ali Izzaldin <ali.izzaldin7@gmail.com>
2024-03-09 11:19:24 +0300

    assignemnt completed

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+   for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+       if (array[i] + array[j] === target) {
+        return true;
+      }
+    }
+  }
+   return false;
 }
 
+console.log(hasTargetSum([3, 8, 12, 4, 11, 7], 10));  
+console.log(hasTargetSum([22, 19, 4, 6, 30], 25));    
+console.log(hasTargetSum([1, 2, 5], 4));              
+console.log(hasTargetSum([-7, 10, 4, 8], 3));         
+console.log(hasTargetSum([1, 2, 3, 4], 5));           
+console.log(hasTargetSum([2, 2, 3, 3], 4));           
+console.log(hasTargetSum([4], 4));                    
+ 
+ 
 /* 
   Write the Big O time complexity of your function here
+  it's O(n^2) you have to iterate twice over the array to find the sum or the target 
 */
 
-/* 
-  Add your pseudocode here
-*/
+ 
 
 /*
   Add written explanation of your solution here
